10490496c1615193aad1a61b966be06b4ce37738
Nikolai Kopperudmoen <nkopperudmoen@gmail.com>
2020-04-23 11:10:59 +0200

    Begynt på admin-GUI
    - La til TableView
    - La til MenuBar

--- src/main/java/org/example/SecondaryController.java
+++ src/main/java/org/example/SecondaryController.java
@@ -1,9 +1,13 @@
 package org.example;
 
 import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class SecondaryController {
+    @FXML
+    private Button secondaryButton;
 
     @FXML
     private void switchToPrimary() throws IOException {
